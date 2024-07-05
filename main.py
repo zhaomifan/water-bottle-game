d6da734f46e13ea1df64f5d60197cbffddeeb15b
赵凯帆 <zhaokf@cheche365.com>
2024-07-05 21:54:06 +0800

    fix:修改参数

--- main.py
+++ main.py
@@ -2,22 +2,17 @@ from typing import List
 
 
 class Bottle:
-    name: int
+    no: str
     volume: int
     water: int
 
-    def __init__(self, volume: int, water: int = 0):
-        # self.no = no
+    def __init__(self, no: str, volume: int, water: int = 0):
+        self.no = no
         self.volume = volume
         self.water = water
 
     def beauty_print(self):
-        print(f"{self.volume}升({self.water}升)")
-
-        b_str = ""
-        for i in range(self.volume):
-            b_str += "水" if i < self.water else "空"
-        print(f"|{b_str}|\n")
+        print(f"{self.no}号瓶({self.volume}升):{self.water}升")
 
 
 def transfer_water(from_bottle: Bottle, to_bottle: Bottle) -> bool:
@@ -35,9 +30,9 @@ def transfer_water(from_bottle: Bottle, to_bottle: Bottle) -> bool:
     return True
 
 
-def is_game_over(bottles: List[Bottle], targetWater: int):
+def is_game_over(bottles: List[Bottle], target_water: int):
     for bottle in bottles:
-        if bottle.water == targetWater:
+        if bottle.water == target_water:
             return True
     return False
 
@@ -47,21 +42,30 @@ def show_bottles(bottles: List[Bottle]):
         bottle.beauty_print()
 
 
+def validate_input(value: int, valid_values):
+    if value not in valid_values:
+        print(f"警告:非法的输入值{value}")
+        return False
+    return True
+
+
 if __name__ == "__main__":
     # 定义三个瓶子
-    bottles = [Bottle(12, 12), Bottle(8), Bottle(5)]
-    target_water = 6
-    print("游戏开始")
+    bottles = [Bottle(1, 8, 8), Bottle(2, 5), Bottle(3, 3)]
+    target_water = 4
+    bottle_nos = [bottle.no for bottle in bottles]
     while True:
-        print("》》》》》》XXXXXXX《《《《《《《")
+        print("-------游戏目标:获得四升水-------")
         show_bottles(bottles)
         if is_game_over(bottles, target_water):
             print("胜利")
             break
-        print("请输入要倒出水的瓶号")
-        from_no = int(input()) - 1
-        print("请输入要倒入水的瓶号")
-        to_no = int(input()) - 1
-        if not transfer_water(bottles[from_no], bottles[to_no]):
+        from_no = int(input("请输入要倒出水的瓶号:"))
+        if not validate_input(from_no, bottle_nos):
+            continue
+        to_no = int(input("请输入要倒入水的瓶号:"))
+        if not validate_input(to_no, bottle_nos):
+            continue
+        if not transfer_water(bottles[from_no - 1], bottles[to_no - 1]):
             print("提示:无效的操作")
     print("游戏结束")
